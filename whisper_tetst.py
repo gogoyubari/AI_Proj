d7560f6a20222894828a929afcb5044e8c4fff27
gogoyubari <1097517+gogoyubari@users.noreply.github.com>
2024-11-29 11:46:35 +0900

    Update whisper_tetst.py
    logger追加
    modelのダウンロード場所指定
    乱数シード固定
    デフォルト言語を"ja"に
    tempuratureパラメータ0.1stepに設定
    コマンド引数エラー処理

--- whisper_tetst.py
+++ whisper_tetst.py
@@ -2,11 +2,17 @@ import os
 os.environ["CT2_VERBOSE"] = "2"
 os.environ["CT2_CUDA_ALLOCATOR"] = "cub_caching"
 os.environ["CT2_CUDA_CACHING_ALLOCATOR_CONFIG"] = "4,3,15,3221225471"
+os.environ["CUDNN_LOGLEVEL_DBG"] = "2"
+os.environ["CUDNN_LOGDEST_DBG"] = "stderr"
 
 import time
 import sys
 from faster_whisper import WhisperModel
 from faster_whisper.vad import VadOptions
+from ctranslate2 import set_random_seed
+import logging
+logging.basicConfig()
+logging.getLogger("faster_whisper").setLevel(logging.DEBUG)
 
 import pysubs2
 from term_printer import Color, cprint
@@ -30,16 +36,20 @@ def whisper(filepath):
         model_size_or_path=model_size_or_path,
         device="cuda",
         compute_type="float16",
+        download_root="model",
     )
+    set_random_seed(0)
 
-    with open('initial_prompt.txt', encoding="utf_8_sig") as f:
+    with open('initial_prompt_J.txt', encoding="utf_8_sig") as f:
         lines = f.read().strip().split('\n')
         init_prompt = ''.join(['<|0.00|> ' + line.strip() + '<|0.00|>' for line in lines])
         print("initial_prompt has been loaded.")
 
     segments, info = model.transcribe(
         audio = filepath,
-        language = "en",
+        language = "ja",
+        log_progress = False,
+        temperature = [n/10 for n in range(11)],
         # beam_size = 5,
         # condition_on_previous_text = True,
         prompt_reset_on_temperature = 0.8,
@@ -81,4 +91,6 @@ if __name__ == '__main__':
         try:
             whisper(args[1])
         except KeyboardInterrupt:
-            pass
+            print("main_thread has been interrupted.")
+        except Exception as e:
+            print(e)
